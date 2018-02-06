02e32f92c7bc284745cbdb9aab602232952d3bbf
Rashik I Nahian <rashik@nahian.me>
2018-02-07 00:22:17 +0600

    config changes

--- user/config.php
+++ user/config.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";    /* Host name */
 $user = "root";         /* User */
-$password = "root";         /* Password */
+$password = "";         /* Password */
 $dbname = "Agridoc";   /* Database name */
 
 // Create connection
