de1f3f257836a925631adad53a131bf412b2dd5e
Rashik I Nahian <rashik@nahian.me>
2018-03-19 11:34:20 +0600

    sql change

--- register.sql
+++ register.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 11, 2018 at 07:20 AM
+-- Generation Time: Mar 19, 2018 at 06:29 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.8
 
@@ -23,6 +23,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `images` (
   `id` int(11) NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   `catagory` varchar(23) COLLATE utf8_unicode_ci NOT NULL,
   `report` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
   `image` longblob NOT NULL,
@@ -108,7 +110,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `tbl_users`
 --
